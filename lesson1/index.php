67e5120d065fddf13eb103f3bb0490098ffcdc19
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-08-16 19:03:25 +0300

    deleted unnecessary data

--- lesson1/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$vardas = 'Petras';
-$pavarde = 'Petraitis';
-$gimimo_metai= 1990;
-$metai=2018;
-$sakinys ='Aš esu '.$vardas.' '.$pavarde.'.  Man yra '.($metai - $gimimo_metai);
-
-echo $sakinys;
-
--- lesson5/inputKlasej/failas2.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-echo'<h1> antras failas </h1>';
